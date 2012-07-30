878dd82e7c2d4c659679ec841e6c1d206aa93b18
Alex Jarvis <alxjrvs@gmail.com>
2012-07-30 01:46:34 -0400

    initial commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DingMe::Application.config.secret_token = '12b106cfa83be16883f89ed28056ade281381fd16a60d872b8b25efd5e2543acef03d539c2afcf6c61ac6f7426bb1a67b8366f3783ad4985e663f2ed13f2f5dc'
